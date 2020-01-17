e4e2b3553b88ebeeb5e59efd6ae40c3851c6c17d
Kevin Jensen <jensenk28@yahoo.com>
2020-01-17 10:10:33 -0800

    made notes explaining what im doing for TL and myself to keep track of where im at in code

--- components/Header/index.js
+++ components/Header/index.js
@@ -10,30 +10,34 @@
 // And add it to the DOM in the .header-container component
 const headerContainer = document.querySelector('.header-container');
 
-const headerElements = Header();
-
-headerContainer.appendChild(headerElements);
-
-
 function Header() {
+    //creat variables to label created elements
     const header = document.createElement('div');
     const date = document.createElement('span');
     const title = document.createElement('h1');
     const temp = document.createElement('span');
 
-    date.classList.add('date');
-    temp.classList.add('temp');
-    header.classList.add('header');
-
-    title.textContent = 'Lambda Times';
-    temp.textContent = '98';
-    date.textContent = 'SMARCH 28 2019';
-
+    //append elements to header div
     header.appendChild(date);
     header.appendChild(title);
     header.appendChild(temp);
 
-    console.log(header);
+    //give elements class names
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
     
+    //give elements text content
+    title.textContent = 'Lambda Times';
+    temp.textContent = '98';
+    date.textContent = 'SMARCH 28, 2019';
+
+    //make sure it looks right in console
+    // console.log(header);
+    
+    //return the created header variable and all the new content appended to it
     return header;
 }
+
+//add the header to the .header-container
+headerContainer.appendChild(Header());
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,45 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+//grab the topics div
+const tabs = document.querySelector
+('.topics')
+
+//send get request
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+  .then((response) => {
+    // handle success
+    // console.log(response);
+    //create variable holding returned data
+    const topics = response.data.topics;
+    //check to see whats in topics
+    // console.log(topics);
+    //cycle through each topic and apply it to the "createTabs" function
+    topics.forEach(topic => {
+        createTabs(topic);
+    })
+  })
+  .catch((error) => {
+    // handle error
+    console.log('error!', error);
+  })
+
+  //create a function that for the tabs
+function createTabs(topic) {
+    //create div
+    const tabDiv = document.createElement('div');
+
+    //add classlist
+    tabDiv.classList.add('tab');
+
+    //add textcontent from topic
+    tabDiv.textContent = topic;
+
+    //add everything to .topics div
+    tabs.appendChild(tabDiv);
+
+    return tabs;
+      
+  }
+  
