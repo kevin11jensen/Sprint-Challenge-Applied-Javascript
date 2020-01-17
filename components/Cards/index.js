93b2f80575fefc93ca81d06e3e70aac984713a37
Kevin Jensen <jensenk28@yahoo.com>
2020-01-17 10:57:00 -0800

    got it to work at the last minute phew

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,15 +17,68 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
-
+//variable for .cards container
+const cards = document.querySelector('.cards-container');
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
 
 axios.get('https://lambda-times-backend.herokuapp.com/articles').then(response => {
-    let cardInfro = response.data.articles;
+    let info = response.data.articles;
     console.log(response.data.articles);
-})
 
 // You will be creating a component for each 'article' in the list.
 
 //articles in list: javascript, bootstrap, technology, jquery, node
 
+Object.values(info.javascript).forEach((item) => {
+    cards.appendChild(cardCreator(item.headline, item.authorPhoto, item.authorName));
+});
+
+Object.values(info.bootstrap).forEach((item) => {
+    cards.appendChild(cardCreator(item.headline, item.authorPhoto, item.authorName));
+});
+
+Object.values(info.technology).forEach((item) => {
+    cards.appendChild(cardCreator(item.headline, item.authorPhoto, item.authorName));
+});
+
+Object.values(info.jquery).forEach((item) => {
+    cards.appendChild(cardCreator(item.headline, item.authorPhoto, item.authorName));
+});
+Object.values(info.node).forEach((item) => {
+    cards.appendChild(cardCreator(item.headline, item.authorPhoto, item.authorName));
+});
+
+})
+.catch(error => {
+    console.log('Error!', error);
+})
+
+function cardCreator(headline, authorPhoto, authorName) {
+    //create elements
+    const card = document.createElement('div'),
+    cardHeadline = document.createElement('div'),
+    cardAuthorDiv = document.createElement('div'),
+    cardImgDiv = document.createElement('div'),
+    cardImg = document.createElement('img'),
+    cardAuthor = document.createElement('span');
+
+        //add classes
+    card.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardAuthorDiv.classList.add('author');
+    cardImgDiv.classList.add('img-container');
+
+    //append
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthorDiv);
+    cardAuthorDiv.appendChild(cardImgDiv);
+    cardImgDiv.appendChild(cardImg);
+    cardAuthorDiv.appendChild(cardAuthor);
+
+    //content
+    cardHeadline.textContent = headline;
+    cardImg.src = authorPhoto;
+    cardAuthor.textContent = authorName;
+
+    return card;
+}
